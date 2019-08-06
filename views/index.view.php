f09fcb1564739010e5ef4f006e333941ca6a46d3
Juan Farber <juantuc98@gmail.com>
2019-08-06 14:35:33 -0300

    Creamos una galeria dinamica de fotos. Realizamos los estilos, las estructuras basicas y descargamos unas imagenes de 500x500 px de la web para mostrarlas en el sitio. En las proximas versiones se realizara la maquetacion d la foto y la opcion de subir otras fotos como usuario para agregarlas a la galeria. Tambien se configuraran las demas paginas en caso de sobrepasar el limite de fotos por pagina y tambien se creara una base de datos.

---
+++ views/index.view.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Slabo+27px&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Galeria</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor">
+            <h1 class="titulo">Mi galeria</h1>
+        </div>
+    </header>
+    <section class="fotos">
+        <div class="contenedor">
+            <div class="thumb">
+                <a href="#">
+                    <img src="imagenes/1.jpeg" alt="">
+                </a>
+            </div>
+
+            <div class="thumb">
+                <a href="#">
+                    <img src="imagenes/2.jpg" alt="">
+                </a>
+            </div>
+
+            <div class="thumb">
+                <a href="#">
+                    <img src="imagenes/3.jpg" alt="">   <!-- Cargamos las imagenes manualmente. -->
+                </a>
+            </div>
+
+            <div class="thumb">
+                <a href="#">
+                    <img src="imagenes/4.jpg" alt="">
+                </a>
+            </div>
+
+            <div class="thumb">
+                <a href="#">
+                    <img src="imagenes/5.jpg" alt="">
+                </a>
+            </div>
+
+            <div class="thumb">
+                <a href="#">
+                    <img src="imagenes/6.jpg" alt="">
+                </a>
+            </div>
+            
+            <div class="thumb">
+                <a href="#">
+                    <img src="imagenes/1.jpeg" alt="">
+                </a>
+            </div>
+
+            <div class="thumb">
+                <a href="#">
+                    <img src="imagenes/2.jpg" alt="">
+                </a>
+            </div>
+
+            <div class="paginacion">
+                <a href="#" class="izquierda"><i class="fa fa-long-arrow-left"></i> Pagina anterior</a>
+                <a href="#" class="derecha">Pagina siguiente <i class="fa fa-long-arrow-right"></i></a>
+            </div>
+        </div>
+    </section>
+        <footer>
+            <p class="copyright">J.F - Agosto de 2019.</p>
+        </footer>
+</body>
+</html>
