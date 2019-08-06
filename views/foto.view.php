d055731adcac64c7c2e6c844c2ef5b639efc0582
Juan Farber <juantuc98@gmail.com>
2019-08-06 15:01:57 -0300

    Se creo el archivo para poder visualizar una foto y el formulario para poder subir imagenes como usuario. En la proxima version crearemos la base de datos y crearemos los enlaces entre los archivos. Tambien se corrijieron errores en los estilos.

---
+++ views/foto.view.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Slabo+27px&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Galeria</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor">
+            <h1 class="titulo">Foto: 1.jpeg</h1>
+        </div>
+    </header>
+
+    <div class="contenedor">
+
+        <div class="foto">
+            <img src="imagenes/1.jpeg" alt="">
+            <p class="texto">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <a href="index.php" class="regresar"><i class="fa fa-long-arrow-left"></i> Regresar</a>
+        </div>
+
+    </div>
+
+    <footer>
+        <p class="copyright">J.F - Agosto de 2019.</p>
+    </footer>
+</body>
+</html>
---
+++ views/subir.view.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Slabo+27px&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Galeria</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor">
+            <h1 class="titulo">Subir foto</h1>
+        </div>
+    </header>
+
+    <div class="contenedor">
+
+    <!-- Formulario para poder subir una foto -->
+
+    <!-- enctype="multipart/form-data" para poder subir archivos. -->
+        <form class="formulario" method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+            <label for="foto">Selecciona tu foto</label>
+            <input type="file" id="foto" name="foto">       <!-- Para poder establecer la foto. -->
+
+            <label for="titulo">Titulo de la foto</label>   <!-- Para poder establecer el titulo. -->
+            <input type="text" id="titulo" name="titulo">
+
+            <label for="texto">Descripcion:</label>         <!-- Establecemos la descripcion -->
+            <textarea name="texto" id="texto" placeholder="Ingresa una descripcion"></textarea>
+
+            <input type="submit" class="submit" value="Subir foto"> <!-- Boton submit. -->
+        </form>
+
+    </div>
+
+    <footer>
+        <p class="copyright">J.F - Agosto de 2019.</p>
+    </footer>
+</body>
+</html>
